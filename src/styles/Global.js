6ce3f2ec0637e8204d50e12b8eeabc4ef9bc3853
Helari <helari.sosi@gmail.com>
2021-01-28 19:36:04 +0100

    Fix mobile overflow

--- src/styles/Global.js
+++ src/styles/Global.js
@@ -1,19 +1,19 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  html {
-    scroll-behavior: smooth;
-  }
   body {
     margin: 20px;
     padding: 0;
     font-family: "Montserrat";
     background-color: #f4f9fc;
     color: #0F1C61;
-
   }
 
   @media (max-width: 768px) {
+    html, body {
+      overflow-x: hidden;
+    }
+    
     body {
       margin: 0;
     }
